c15b045625e1f6473c7ef43a4a1b57703cc1db85
Omnia <omniaeid314@gmail.com>
2024-08-11 22:56:07 +0300

    the wecomemessage added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,12 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
+import Header from './components/Header.jsx'
+import MainContent from './components/MainContent.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      {/* <Header />
+      <MainContent />
+      <Footer /> */}
       <WelcomeMessage />
 
       <div>
---
+++ alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+    return (
+     
+            <footer>
+            <p>© 2023 City Lovers</p>
+            </footer>
+    );
+}
+
+export default WelcomeMessage;
