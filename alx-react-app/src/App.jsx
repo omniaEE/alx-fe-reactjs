739519c232b86a28085443737911174b24dbadc8
Omnia <omniaeid314@gmail.com>
2024-08-11 22:41:47 +0300

    the wecomemessage added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './WelcomeMessage.jsx'
+import WelcomeMessage from './components/WelcomeMessage.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
