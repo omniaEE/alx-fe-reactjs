eb650cd1622aed5dba7ab1fd7cbad1b73701f86d
Omnia <omniaeid314@gmail.com>
2024-08-11 23:01:01 +0300

    the wecomemessage added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,15 +6,17 @@ import WelcomeMessage from './components/WelcomeMessage.jsx'
 import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
+import UserProfile from './components/UserProfile.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <Header />
+      <Header />
       <MainContent />
-      <Footer /> */}
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Footer />
       <WelcomeMessage />
 
       <div>
