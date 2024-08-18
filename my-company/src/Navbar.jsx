28e455e318fa2e8e22425ecd52a2646e835164b0
Omnia <omniaeid314@gmail.com>
2024-08-18 22:37:58 +0300

    the wecomemessage added

---
+++ my-company/src/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{ display: 'flex', justifyContent: 'space-between', padding: '10px' }}>
+      <h1>My Company</h1>
+      <ul style={{ display: 'flex', listStyle: 'none' }}>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
