dbf2b6ec3f7b43412dc5dc7ec09dc9ac6b7aa8e5
Omnia <omniaeid314@gmail.com>
2024-08-18 22:22:09 +0300

    the wecomemessage added

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,6 +3,7 @@ import ProfilePage from './components/ProfilePage';
 import { UserProvider } from './components/UserContext';
 
 function App() {
+  ["UserContext.Provider", "value"]
   return (
     <UserProvider>
       <ProfilePage />
