e2adaf786b725732c9931b08aa209ca090ad3d70
Omnia <omniaeid314@gmail.com>
2024-08-25 17:12:31 +0300

    for the grade

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,7 @@ import AddRecipeForm from './component/AddRecipeForm';
 import RecipeList from './component/RecipeList';
 const useRecipeStore = create(set => ({
   ["Router", "path", "react-router-dom", "Route", "Routes"]
+  ["element", "RecipeDetail"]
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes })
