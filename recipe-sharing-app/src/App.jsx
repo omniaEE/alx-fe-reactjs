244330245b48bc693790258c25ca621cc50bc64d
Omnia <omniaeid314@gmail.com>
2024-08-25 16:35:11 +0300

    the wecomemessage added

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,6 @@
 import create from 'zustand'
-
+import AddRecipeForm from './component/AddRecipeForm';
+import RecipeList from './component/RecipeList';
 const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
