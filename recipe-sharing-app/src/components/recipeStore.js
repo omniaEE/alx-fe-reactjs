e2c885d542123738b9c71e616e6a507472a6e2c1
Omnia <omniaeid314@gmail.com>
2024-08-25 17:07:59 +0300

    for the grade

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,8 +1,13 @@
-import create from "zustand";
+import create from 'zustand';
 
-const useRecipeStore = create((set) => ({
-  recipes: ["updateRecipe", "deleteRecipe"],
-  addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
+const useRecipeStore = create(set => ({
+  recipes: [],
+  searchTerm: '',
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filteredRecipes: [],
+  filterRecipes: () => set(state => ({
+    filteredRecipes: state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    ))
+  })),
 }));
