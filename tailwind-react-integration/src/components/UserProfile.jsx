2d315cd682727ba61209f34d392938f2ae460903
Omnia <omniaeid314@gmail.com>
2024-09-15 20:44:49 +0300

    first commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
     return (
-      <div className="user-profile">
-        <img src="https://via.placeholder.com/150" alt="User" />
-        <h1>John Doe</h1>
-        <p>Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <div className="userProfile bgGray100 p8 maxWSm mxAuto my20 roundedLg shadowLg">
+        <img className="roundedFull w36 h36 mxAuto" src="https://via.placeholder.com/150" alt="User" />
+        <h1 className="textXl textBlue800 my4">John Doe</h1>
+        <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
   }
