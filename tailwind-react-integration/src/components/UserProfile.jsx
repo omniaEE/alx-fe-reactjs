c83d034ac4f6c9a518c7ba07c7e464beb48e6d3e
Omnia <omniaeid314@gmail.com>
2024-09-15 20:48:46 +0300

    first commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -9,4 +9,5 @@ function UserProfile() {
   }
   ["bg-gray-100", "p-8", "max-w-sm", "mx-auto", "my-20", "rounded-lg", "shadow-lg"]
   ["rounded-full", "w-36", "h-36"]
+  ["text-xl", "text-blue-800", "my-4"]
   export default UserProfile;
