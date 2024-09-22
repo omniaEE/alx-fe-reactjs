1204f0ebbbc02cddcb2cc40d8c9e3bf5fcd85bd5
Omnia <omniaeid314@gmail.com>
2024-09-22 12:34:53 +0300

    first commit

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -16,3 +16,17 @@ function App() {
 }
 
 export default App
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
+  );
+}
