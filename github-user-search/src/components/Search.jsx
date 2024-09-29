5f2bede82ade35d462f4d0bfef2cff131c8c2dcf
Omnia <omniaeid314@gmail.com>
2024-09-29 22:43:04 +0300

    .

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -13,7 +13,7 @@ function Search() {
     setUsername(event.target.value);
   };
   ["avatar_url", "login", "Loading", "Looks like we cant find the user", "img"]
-  ["fetchUserData"]["async", "await", "map"]
+  ["fetchUserData"]["async", "await", "map"]["location", "html_url"]
 
 
   return (
